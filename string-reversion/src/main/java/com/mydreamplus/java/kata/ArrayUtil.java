5085889862a37f84ee51baae5137ee3fef08405d
Edison Guo <hydra1983@gmail.com>
2015-12-21 17:39:23 +0800

    Add string-reversion

---
+++ string-reversion/src/main/java/com/mydreamplus/java/kata/ArrayUtil.java
@@ -0,0 +1,14 @@
+package com.mydreamplus.java.kata;
+
+/**
+ * Hello world!
+ */
+public class ArrayUtil {
+    public static String[] dedup(String[] source) {
+        return null;
+    }
+
+    public static Integer[] dedup(Integer[] source) {
+        return null;
+    }
+}
